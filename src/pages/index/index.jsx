ac3a2a7714abb29e81dc540769acc4da5494e68d
Explorer-Wu <wuwenhan@WuWhs-MacBook-Pro.local>
2020-11-16 15:30:35 +0800

    最新版H5适配，首页修改和taro-ui样式引入

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import { View, Text } from '@tarojs/components'
-import { AtButton } from 'taro-ui'
+import { AtNavBar, AtButton } from 'taro-ui'
 
-import "taro-ui/dist/style/components/button.scss" // 按需引入
+// import "taro-ui/dist/style/components/button.scss" // 按需引入
 import './index.scss'
 
 export default class Index extends Component {
@@ -20,6 +20,17 @@ export default class Index extends Component {
   render () {
     return (
       <View className='index'>
+        <AtNavBar
+          onClickRgIconSt={this.handleClick}
+          onClickRgIconNd={this.handleClick}
+          onClickLeftIcon={this.handleClick}
+          color='#000'
+          title='NavBar 导航栏示例'
+          leftText='返回'
+          rightFirstIconType='bullet-list'
+          rightSecondIconType='user'
+        />
+
         <Text>Hello world!</Text>
         <AtButton type='primary'>I need Taro UI</AtButton>
         <Text>Taro UI 支持 Vue 了吗？</Text>
